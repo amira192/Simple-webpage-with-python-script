e30dd94222c8a8b14ae0e0f8c80e144abf111c72
amira192 <amira17_6@hotmail.com>
2025-08-30 15:36:45 +0300

    initial commit

---
+++ calculation.py
@@ -0,0 +1,10 @@
+x=10
+y=80
+print("Add: {}".format(x + y))
+print("Sub: {}".format(x-y))
+print("Mult: {}".format(x*y))
+print("Div: {}".format(x/y))
+if x > y:
+     print("x is bigger" )
+else:
+     print(" x is smaller")
